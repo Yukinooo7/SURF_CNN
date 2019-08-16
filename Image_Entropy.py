56bf5004df1cd45e68d3eb2b80af7268b4af81b6
Xunjie <634629477@qq.com>
2019-08-16 16:34:55 +0800

    Add test dataset for CNN

--- Image_Entropy.py
+++ Image_Entropy.py
@@ -1,24 +1,27 @@
 import cv2
 import numpy as np
 import math
+from Util import ero_dila
 
 image = cv2.imread('E:\\Study\\2019-Summer\\SURF\\CNN\\data\\images\\20190808\\0808170829.jpg', 0)
 chaos = cv2.imread('E:\\Study\\2019-Summer\\SURF\\CNN\\data\\images\\20190812140738.jpg', 0)
 grid = cv2.imread('E:\\Study\\2019-Summer\\SURF\\CNN\\data\\grid.png', 0)
-_, thresh = cv2.threshold(grid, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
-cv2.imshow("SS", thresh)
-cv2.waitKey(0)
+
+
+# _, thresh = cv2.threshold(grid, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
+# cv2.imshow("SS", thresh)
+# cv2.waitKey(0)
+
 
 def get_entropy(img):
     ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
+    thresh = ero_dila(thresh, 5)
     tmp = []
     for i in range(256):
         tmp.append(0)
     val = 0
     k = 0
     res = 0
-
-
 
     img = np.array(thresh)
     for i in range(len(img)):
@@ -40,3 +43,8 @@ def get_entropy(img):
 print(get_entropy(image))
 print(get_entropy(chaos))
 print(get_entropy(grid))
+
+cv2.imshow("Image", image)
+cv2.imshow("Chaos", chaos)
+
+cv2.waitKey(0)
--- get_diameter.py
+++ get_diameter.py
@@ -1,28 +1,52 @@
 import cv2
-from Util import ero_dila
+from Util import ero_dila, mean
+from matplotlib import pyplot
+
+image = cv2.imread('E:\\Study\\2019-Summer\\SURF\\CNN\\data\\images\\20190813151052.jpg', 0)
+
+_, img = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
+img = ero_dila(img, 1)
+
+
+def get_diagram(x, y):
+    # 生成图表
+    pyplot.plot(x, y)
+    # # 设置横坐标为year，纵坐标为population，标题为Population year correspondence
+    # pyplot.xlabel('year')
+    # pyplot.ylabel('population')
+    # pyplot.title('Population year correspondence')
+    # 设置纵坐标刻度
+    pyplot.yticks([0, 25, 50, 75, 90])
+    # 设置填充选项：参数分别对应横坐标，纵坐标，纵坐标填充起始值，填充颜色（可以有更多选项）
+    pyplot.fill_between(x, y, 0, color='green')
+    # 显示图表
+    pyplot.show()
 
 
 def get_all_diameters(img):
-    for i in range(img.shape[0]):
-        get_diameter(img, i)
+    for i in range(1, img.shape[0], 15):
+        dia_list = get_diameter(img, i)
+        if len(dia_list) > 0:
+            print(len(dia_list))
 
 
-def get_diameter(img, y_axis):
+def get_diameter(input_img, y_axis):
     dia_list = []
     diameter = 0
     flag = False
     line_flag = 0
-    _, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
-    img = ero_dila(img, 1)
+    _, input_img = cv2.threshold(input_img, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
+    input_img = ero_dila(input_img, 1)
 
-    for i in range(img.shape[1]):
-        if img[y_axis][i] < 10:
+    for i in range(input_img.shape[1]):
+        if input_img[y_axis][i] < 10:
             flag = True
             line_flag = line_flag + 1
         else:
             flag = False
             line_flag = 0
-            if diameter != 0:
+            if diameter > 5:
+                cv2.circle(img, (i, y_axis), 5, (0, 0, 0), 1)
                 dia_list.append(diameter)
             diameter = 0
 
@@ -34,16 +58,11 @@ def get_diameter(img, y_axis):
 
             break
 
-    print(dia_list)
+    return dia_list
 
 
 if __name__ == "__main__":
-    image = cv2.imread('E:\\Study\\2019-Summer\\SURF\\CNN\\data\\images\\20190813151052.jpg', 0)
-
-    get_all_diameters(image)
-
-    _, img = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)  # 获取灰度图
-    img = ero_dila(img, 1)
+    get_all_diameters(img)
 
     cv2.imshow("Origin", image)
 
